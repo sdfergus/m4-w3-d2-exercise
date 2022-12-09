e483163bfa646f038101634ac60d6ce1fa4fe8c5
Shandira <shandira.ferguson@gmail.com>
2022-12-08 19:13:24 -0800

    Exercise complete

---
+++ express-helmet-app/app.js
@@ -0,0 +1,16 @@
+// @ts-nocheck
+'use strict'
+
+const express = require( 'express' );
+const helmet = require( 'helmet' );
+const app = express();
+
+app.use( helmet() ); //to protect the header
+
+app.get( '/', ( req, res ) => {
+  console.log( 'route \'/\' called!' );
+  res.send( { res: 'Sending 200 as response' } );
+} );
+
+app.listen( 3000 );
+console.log( 'Express app is up and running on port 3000!' );
